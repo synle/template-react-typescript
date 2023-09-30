1b90c787389462f30761efa000b87394ecc41c58
Sy Le <lenguyensy@gmail.com>
2023-09-30 15:12:53 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ sed -i "s/template-react-typescript-webpack/$NEW_PROJECT_NAME/" src/components/H
 echo "# $NEW_PROJECT_NAME" > README.md
 
 # clean up the sh file
-rm setup.sh testLib.js
+rm -rf setup.sh
